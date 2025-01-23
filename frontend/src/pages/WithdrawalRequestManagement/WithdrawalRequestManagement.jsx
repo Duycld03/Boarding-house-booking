5172cc8602b6adc065cc58e335d08643814e4555
HongY1402 <todohongy@gmail.com>
2025-01-23 20:59:18 +0700

    fix_review_2

--- frontend/src/pages/WithdrawalRequestManagement/WithdrawalRequestManagement.jsx
+++ frontend/src/pages/WithdrawalRequestManagement/WithdrawalRequestManagement.jsx
@@ -8,23 +8,13 @@ import {
 import { toast } from 'react-toastify';
 import { Tag } from 'antd';
 import { getWithdrawRequests } from '../../api/withdrawalrequestmanagement';
+import formatAmount from '../../utils/formatAmount';
 
 function WithdrawalRequestManagement() {
   const statusColors = {
     pending: 'orange',
     processed: 'green',
     cancelled: 'red',
-  };
-
-  const formatAmount = (amount) => {
-    if (amount >= 1e9) {
-      return (amount / 1e9).toFixed(1) + 'B';
-    } else if (amount >= 1e6) {
-      return (amount / 1e6).toFixed(1) + 'M';
-    } else if (amount >= 1e3) {
-      return (amount / 1e3).toFixed(1) + 'K';
-    }
-    return amount;
   };
 
   const columns = [
