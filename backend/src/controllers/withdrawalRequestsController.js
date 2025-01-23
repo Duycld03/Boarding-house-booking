049bbbff448e8539ae68b767ef143795e1c5060e
HongY1402 <todohongy@gmail.com>
2025-01-23 07:28:43 +0700

    update

--- backend/src/controllers/withdrawalRequestsController.js
+++ backend/src/controllers/withdrawalRequestsController.js
@@ -3,16 +3,15 @@ import withdrawRequest from '../models/withdrawRequest.js';
 class withdrawalRequestsController {
   async getWithdrawRequests(req, res) {
     try {
-      // Populate userId and processedBy to fetch fullname from Account model
       const withdrawRequests = await withdrawRequest
         .find()
         .populate({
-          path: 'userId', // Populate userId
-          select: 'fullname', // Chỉ lấy trường fullname
+          path: 'userId',
+          select: 'fullname',
         })
         .populate({
-          path: 'processedBy', // Populate processedBy
-          select: 'fullname', // Chỉ lấy trường fullname
+          path: 'processedBy',
+          select: 'fullname',
         });
 
       return res.status(200).json(withdrawRequests);
