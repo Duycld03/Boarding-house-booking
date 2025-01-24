5b55948e2e28886ab2fcfde926fd4ae8584077e4
HongY1402 <todohongy@gmail.com>
2025-01-24 15:51:06 +0700

    BE_delete_review_report

--- backend/src/controllers/reportController.js
+++ backend/src/controllers/reportController.js
@@ -19,6 +19,28 @@ class reportController {
       return res.status(500).json({ error: error.message });
     }
   }
+  async softDeleteReport(req, res) {
+    try {
+      const { reviewReportId } = req.params;
+
+      // Tìm và cập nhật trường "deleted" thành true
+      const report = await Report.findByIdAndUpdate(
+        reviewReportId,
+        { deleted: true },
+        { new: true } // Để trả về bản ghi đã cập nhật
+      );
+
+      if (!report) {
+        return res.status(404).json({ error: 'Report not found' });
+      }
+
+      return res
+        .status(200)
+        .json({ message: 'Report soft deleted successfully', report });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
+    }
+  }
 }
 
 export default new reportController();
--- backend/src/routes/reportRoutes.js
+++ backend/src/routes/reportRoutes.js
@@ -5,5 +5,9 @@ import { reportController } from '../controllers/index.js';
 const adminRouter = Router();
 
 adminRouter.get('/reviewreports', reportController.getReviewReports);
+adminRouter.delete(
+  '/reviewreports/:reviewReportId',
+  reportController.softDeleteReport
+);
 
 export default adminRouter;
