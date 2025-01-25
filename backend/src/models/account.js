846f7d5ab4c89b65665734f66c67f8ba78c1928c
phucdtWork <phucdt.work@gmail.com>
2025-01-26 00:13:10 +0700

    feat: view detail and update account

--- backend/src/models/account.js
+++ backend/src/models/account.js
@@ -55,12 +55,13 @@ const AccountSchema = mongoose.Schema(
     },
     status: {
       type: String,
+      default: 'inactive'
     },
   },
   { timestamps: true }
 );
 
-AccountSchema.plugin(mongoose_delete, { deletedBy: true, overrideMethods: 'all' });
+AccountSchema.plugin(mongoose_delete, { overrideMethods: 'all' });
 
 const Account = mongoose.model("Account", AccountSchema);
 export default Account;
