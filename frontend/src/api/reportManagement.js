36123ac831689672ffdb455fb0fcc785e2753dcb
HongY1402 <todohongy@gmail.com>
2025-01-25 16:09:21 +0700

    fix_api_fe_getReviewReport

--- frontend/src/api/reportManagement.js
+++ frontend/src/api/reportManagement.js
@@ -1,7 +1,7 @@
 import axios from './axios.config';
 
 export const getReviewReports = () => {
-  return axios.get('/dashboard/reviewreports');
+  return axios.get('/dashboard/review-reports');
 };
 export const deleteReport = (reportId) => {
   return axios.delete(`/dashboard/reports/${reportId}`);
