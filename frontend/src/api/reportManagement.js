2341627df102186110c6470899e79e33f3cbd053
HongY1402 <134051685+HongY1402@users.noreply.github.com>
2025-01-25 14:22:40 +0700

    Update reportManagement.js

--- frontend/src/api/reportManagement.js
+++ frontend/src/api/reportManagement.js
@@ -1,15 +1,15 @@
 import axios from './axios.config';
 
 export const getReviewReports = () => {
+  return axios.get("/dashboard/reviewreports");
+};
+export const deleteReviewReport = (reviewReportId) => {
+  return axios.delete(`/dashboard/reviewreports/${reviewReportId}`);
+};
 export const sendReportReplyByEmail = (reviewReportId, data) => {
   return axios.put(
     `/dashboard/review-reports/${reviewReportId}/send-email`,
     data
   );
-};
-  return axios.get("/dashboard/reviewreports");
-};
-export const deleteReviewReport = (reviewReportId) => {
-  return axios.delete(`/dashboard/reviewreports/${reviewReportId}`);
 };
 
