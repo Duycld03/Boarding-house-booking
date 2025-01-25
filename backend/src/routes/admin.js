f1008c3b8c74db23ea21c75263855f9e7a60e22a
phucdtWork <phucdt.work@gmail.com>
2025-01-25 09:47:52 +0700

    fix: fix conflict when merge feature

--- backend/src/routes/admin.js
+++ backend/src/routes/admin.js
@@ -1,8 +1,12 @@
 import { Router } from 'express';
 
 import {
+
+  withdrawalRequestsController,
+  AccountController,
   BoardingHouseController,
-  withdrawalRequestsController
+  reportController,
+
 } from '../controllers/index.js';
 
 const adminRouter = Router();
@@ -11,9 +15,17 @@ adminRouter.get(
   '/withdrawRequests',
   withdrawalRequestsController.getWithdrawRequests
 );
+adminRouter.get('/reviewreports', reportController.getReviewReports);
+adminRouter.delete(
+  '/reviewreports/:reviewReportId',
+  reportController.softDeleteReport
+);
 
 adminRouter.get('/boardinghouse', BoardingHouseController.getAllBHOnDashBoard)
 
-
+//Account
+adminRouter.get('/account', AccountController.getAllAccount)
+adminRouter.delete('/account/:accountId', AccountController.softDeleteAccount)
+adminRouter.get('/account/filter', AccountController.filterAccounts);
 
 export default adminRouter;
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -5,6 +5,7 @@ import {
 } from '../middlewares/index.js';
 import { apiRouter } from './api.js';
 import adminRouter from './admin.js';
+
 function routes(app) {
   app.use('/api', apiRouter);
   app.use('/dashboard/admin', adminRouter);
