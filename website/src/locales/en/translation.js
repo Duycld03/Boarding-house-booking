a520ad53ac849b98e56bea64e7f0e09fed419df4
HongY1402 <todohongy@gmail.com>
2025-05-07 18:18:18 +0700

    feat(blog): add config language for website

---
+++ website/src/locales/en/translation.js
@@ -0,0 +1,14 @@
+const en = {
+  menu: {
+    home: 'Home',
+    about: 'About Us',
+    contact: 'Contact',
+    profile: 'Profile',
+    changePassword: 'Change Password',
+    darkMode: 'Dark Mode',
+    lightMode: 'Light Mode',
+    logout: 'Logout',
+  },
+};
+
+export default en;
