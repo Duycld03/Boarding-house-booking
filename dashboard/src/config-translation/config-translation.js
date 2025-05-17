1acc27239d1f5f21d1f1bd8ecd8a590456b5055c
HongY1402 <todohongy@gmail.com>
2025-05-17 08:57:15 +0700

    feat(blog): add language and dark mode for delete

--- dashboard/src/config-translation/config-translation.js
+++ dashboard/src/config-translation/config-translation.js
@@ -6,6 +6,8 @@ import en_accountManagement from '@/locales/en/accountManagement.json';
 import vi_accountManagement from '@/locales/vi/accountManagement.json';
 import en_changePassword from '@/locales/en/changePassword.json';
 import vi_changePassword from '@/locales/vi/changePassword.json';
+import en_reviewManagement from '@/locales/en/reviewManagement.json';
+import vi_reviewManagement from '@/locales/vi/reviewManagement.json';
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -13,11 +15,13 @@ i18n.use(initReactI18next).init({
       menu: en_menu,
       accountManagement: en_accountManagement,
       changePassword: en_changePassword,
+      reviewManagement: en_reviewManagement,
     },
     vi: {
       menu: vi_menu,
       accountManagement: vi_accountManagement,
       changePassword: vi_changePassword,
+      reviewManagement: vi_reviewManagement,
     },
   },
   lng: 'en',
