1ba57da216d2f0fa5723333b0cb635e2469cab2b
HongY1402 <todohongy@gmail.com>
2025-06-03 17:24:27 +0700

    feat(blog): add done call api watch later for mobile

--- mobile/src/app/(screens)/(accounts)/watchlater/index.js
+++ mobile/src/app/(screens)/(accounts)/watchlater/index.js
@@ -1,106 +1,99 @@
-import React, { useState } from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ActivityIndicator } from 'react-native';
 import ListCard from '@/components/ui/ListCard';
 import { BackHeader } from '@/components/navigation/CustomHeader';
-
-const mockData = [
-  {
-    _id: '1',
-    name: 'A',
-    rating: 4,
-    images: [],
-    address: {},
-    boardingHouseType: 'apartment',
-  },
-  {
-    _id: '2',
-    name: 'B',
-    rating: 3,
-    images: [],
-    address: {},
-    boardingHouseType: 'apartment',
-  },
-  {
-    _id: '3',
-    name: 'C',
-    rating: 5,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-  {
-    _id: '4',
-    name: 'D',
-    rating: 2,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-  {
-    _id: '5',
-    name: 'E',
-    rating: 3,
-    images: [],
-    address: {},
-    boardingHouseType: 'apartment',
-  },
-  {
-    _id: '6',
-    name: 'F',
-    rating: 4,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-  {
-    _id: '7',
-    name: 'F',
-    rating: 4,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-  {
-    _id: '8',
-    name: 'F',
-    rating: 4,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-  {
-    _id: '9',
-    name: 'F',
-    rating: 4,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-  {
-    _id: '10',
-    name: 'F',
-    rating: 4,
-    images: [],
-    address: {},
-    boardingHouseType: 'homestay',
-  },
-];
+import { getAllWatchLater, deleteWatchLater } from '@/API/watchLaterManagement';
+import { ConfirmModal } from '@/components/feedback';
+import { useTheme } from '@/context/ThemeProvider';
+import { useTranslation } from 'react-i18next';
+import { useRouter } from 'expo-router';
 
 function WatchLater() {
+  const [watchList, setWatchList] = useState([]);
   const [selectedId, setSelectedId] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [isOpenDeleteModal, setIsOpenDeleteModal] = useState(false);
+  const [pagination, setPagination] = useState({
+    currentPage: 1,
+    totalPages: 1,
+  });
+  const router = useRouter();
+  const { isDarkMode } = useTheme();
+  const { t } = useTranslation('common');
 
-  const handleConfirmDelete = () => {
-    console.log('Deleting item with id:', selectedId);
+  const fetchWatchList = async (page = 1) => {
+    setLoading(true);
+    try {
+      const res = await getAllWatchLater({ page, limit: 8 });
+      setWatchList(res?.data || []);
+      setPagination({
+        currentPage: res?.pagination?.currentPage || 1,
+        totalPages: res?.pagination?.totalPages || 1,
+      });
+    } catch (error) {
+      setLoading(false);
+      router.push('/login');
+    } finally {
+      setLoading(false);
+    }
   };
 
+  const handleConfirmDelete = () => {
+    setIsOpenDeleteModal(true);
+  };
+
+  const handleDelete = async () => {
+    try {
+      await deleteWatchLater(selectedId);
+      fetchWatchList(pagination.currentPage);
+    } catch (error) {
+      console.error('Delete failed:', error);
+    } finally {
+      setIsOpenDeleteModal(false);
+      setSelectedId(null);
+    }
+  };
+
+  useEffect(() => {
+    fetchWatchList();
+  }, []);
+
   return (
-    <View style={{ flex: 1, paddingTop: 16 }}>
-      <BackHeader title="Watch later" />
-      <ListCard
-        data={mockData}
-        onConfirmDelete={handleConfirmDelete}
-        setSelectedId={setSelectedId}
-        mode="favorite"
+    <View
+      style={{
+        flex: 1,
+        paddingTop: 16,
+        backgroundColor: isDarkMode ? '#0f172a' : '#ffffff',
+      }}
+    >
+      <BackHeader title={t('watchLater')} />
+      {loading ? (
+        <View
+          style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+        >
+          <ActivityIndicator size="large" color="#0ea5e9" />
+        </View>
+      ) : (
+        <ListCard
+          data={watchList}
+          onConfirmDelete={handleConfirmDelete}
+          setSelectedId={setSelectedId}
+          mode="watchLater"
+          currentPage={pagination.currentPage}
+          totalPages={pagination.totalPages}
+          onPageChange={(page) => fetchWatchList(page)}
+        />
+      )}
+
+      <ConfirmModal
+        visible={isOpenDeleteModal}
+        title={t('confirmDeletion')}
+        message={t('confirmRemoveWatchLater')}
+        onConfirm={handleDelete}
+        onClose={() => {
+          setIsOpenDeleteModal(false);
+          setSelectedId(null);
+        }}
       />
     </View>
   );
