fb715252b7cb8b7e8e009d76e6b45d47ea175c14
Duycld03 <truongduy2003@gmail.com>
2025-06-05 16:09:07 +0700

    chore: add language for dashboard profile

--- dashboard/src/config-translation/config-translation.js
+++ dashboard/src/config-translation/config-translation.js
@@ -1,19 +1,21 @@
-import i18n from 'i18next';
-import { initReactI18next } from 'react-i18next';
-import en_menu from '@/locales/en/menu.json';
-import vi_menu from '@/locales/vi/menu.json';
-import en_accountManagement from '@/locales/en/accountManagement.json';
-import vi_accountManagement from '@/locales/vi/accountManagement.json';
-import en_changePassword from '@/locales/en/changePassword.json';
-import vi_changePassword from '@/locales/vi/changePassword.json';
-import en_reviewReportManagement from '@/locales/en/reviewReportManagement.json';
-import vi_reviewReportManagement from '@/locales/vi/reviewReportManagement.json';
-import en_reviewManagement from '@/locales/en/reviewManagement.json';
-import vi_reviewManagement from '@/locales/vi/reviewManagement.json';
-import en_reportBoardingHouse from '@/locales/en/reportBoardingHouse.json';
-import vi_reportBoardingHouse from '@/locales/vi/reportBoardingHouse.json';
-import en_common from '@/locales/en/common.json';
-import vi_common from '@/locales/vi/common.json';
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import en_menu from "@/locales/en/menu.json";
+import vi_menu from "@/locales/vi/menu.json";
+import en_accountManagement from "@/locales/en/accountManagement.json";
+import vi_accountManagement from "@/locales/vi/accountManagement.json";
+import en_changePassword from "@/locales/en/changePassword.json";
+import vi_changePassword from "@/locales/vi/changePassword.json";
+import en_reviewReportManagement from "@/locales/en/reviewReportManagement.json";
+import vi_reviewReportManagement from "@/locales/vi/reviewReportManagement.json";
+import en_reviewManagement from "@/locales/en/reviewManagement.json";
+import vi_reviewManagement from "@/locales/vi/reviewManagement.json";
+import en_reportBoardingHouse from "@/locales/en/reportBoardingHouse.json";
+import vi_reportBoardingHouse from "@/locales/vi/reportBoardingHouse.json";
+import en_common from "@/locales/en/common.json";
+import vi_common from "@/locales/vi/common.json";
+import en_profile from "@/locales/en/profile.json";
+import vi_profile from "@/locales/vi/profile.json";
 
 i18n.use(initReactI18next).init({
   resources: {
@@ -25,6 +27,7 @@ i18n.use(initReactI18next).init({
       reviewManagement: en_reviewManagement,
       reportBoardingHouse: en_reportBoardingHouse,
       common: en_common,
+      profile: en_profile,
     },
     vi: {
       menu: vi_menu,
@@ -34,12 +37,13 @@ i18n.use(initReactI18next).init({
       reviewManagement: vi_reviewManagement,
       reportBoardingHouse: vi_reportBoardingHouse,
       common: vi_common,
+      profile: vi_profile,
     },
   },
-  lng: 'en',
-  fallbackLng: 'en',
-  ns: ['menu'],
-  defaultNS: 'menu',
+  lng: "en",
+  fallbackLng: "en",
+  ns: ["menu"],
+  defaultNS: "menu",
   interpolation: {
     escapeValue: false,
   },
