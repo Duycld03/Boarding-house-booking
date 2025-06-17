c1582ddd3eb5a66a0f01fa35e145688998a2a7b5
phucdtWork <phucdt.work@gmail.com>
2025-06-17 09:49:02 +0700

    feat: leak api in dashboard and account filter UI

--- dashboard/src/api/accountAPI.js
+++ dashboard/src/api/accountAPI.js
@@ -1,17 +1,21 @@
 import axios from "./axios.config";
-export const getAllAccount = () => {
-  return axios.get("/dashboard/account");
+export const getAllAccount = (paginationOptions = {}) => {
+  return axios.get("/dashboard/account", { params: paginationOptions });
 };
 
 export const deleteAccount = (accountId) => {
   return axios.delete(`/dashboard/account/${accountId}`);
 };
 
-export const filterAccount = (filterValue) => {
-  return axios.get(`/dashboard/account/filter`, {
-    params: filterValue,
-  });
+export const filterAccount = (filterValue = {}, paginationOptions = {}) => {
+  const params = {
+    ...filterValue,
+    ...paginationOptions,
+  };
+
+  return axios.get(`/dashboard/account/filter`, { params });
 };
+
 
 export const createAccount = (accountData) => {
   return axios.post(`/dashboard/account/create`, accountData);
