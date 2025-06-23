b82d781e3fb4da64e4820d07927f8f6d34b4d0d3
HongY1402 <todohongy@gmail.com>
2025-06-23 10:46:45 +0700

    feat(blog): add BE seposit list

--- website/src/pages/common/DepositManagement/DepositRoom.jsx
+++ website/src/pages/common/DepositManagement/DepositRoom.jsx
@@ -1,18 +1,18 @@
-import React, { useState, useEffect } from "react";
-import { Tag, Input, Modal, Form } from "antd";
-import { toast } from "react-toastify";
+import React, { useState, useEffect } from 'react';
+import { Tag, Input, Modal, Form } from 'antd';
+import { toast } from 'react-toastify';
 import {
   getDepositByBhId,
   acceptDepositRoom,
   rejectDepositRoom,
-} from "../../../../api/depositAPI";
-import { getRoomsByBoardingHouse } from "@/api/roomAPI";
-import { useParams } from "react-router-dom";
-import Table from "@/component/Table";
-import formatAmount from "@/utils/formatAmount";
-import { Button } from "@/component";
-import ConfirmModal from "@/component/ConfirmModal";
-import FilterDeposit from "./FilterDeposite";
+} from '@/api/depositAPI';
+import { getRoomsByBoardingHouse } from '@/api/roomAPI';
+import { useParams } from 'react-router-dom';
+import Table from '@/component/Table';
+import formatAmount from '@/utils/formatAmount';
+import { Button } from '@/component';
+import ConfirmModal from '@/component/ConfirmModal';
+import FilterDeposit from './FilterDeposite';
 
 const DepositRoom = () => {
   const [depositedRooms, setDepositedRooms] = useState([]);
@@ -21,7 +21,7 @@ const DepositRoom = () => {
   const [confirmLoading, setConfirmLoading] = useState(false); // State to manage loading in confirm modal
   const [selectedRoom, setSelectedRoom] = useState(null); // Store selected room for accept action
   const [isRejectModalOpen, setIsRejectModalOpen] = useState(false); // State for reject modal visibility
-  const [reasonForCancel, setReasonForCancel] = useState(""); // Store rejection reason
+  const [reasonForCancel, setReasonForCancel] = useState(''); // Store rejection reason
   const [rejectLoading, setRejectLoading] = useState(false); // State to manage reject button loading state
   const { boardingHouseId } = useParams();
   const [filterValue, setFilterValue] = useState(null);
@@ -33,7 +33,7 @@ const DepositRoom = () => {
       const response = await getRoomsByBoardingHouse(boardingHouseId);
       setListRoom(Array.isArray(response) ? response : []);
     } catch (error) {
-      toast.error("Failed to fetch deposit rooms");
+      toast.error('Failed to fetch deposit rooms');
     } finally {
       setLoading(false);
     }
@@ -44,8 +44,8 @@ const DepositRoom = () => {
       const response = await getDepositByBhId(boardingHouseId, filterValue);
       setDepositedRooms(Array.isArray(response) ? response : []);
     } catch (error) {
-      console.error("Error fetching deposit rooms:", error);
-      toast.error("Failed to fetch deposit rooms");
+      console.error('Error fetching deposit rooms:', error);
+      toast.error('Failed to fetch deposit rooms');
     } finally {
       setLoading(false);
     }
@@ -65,7 +65,7 @@ const DepositRoom = () => {
 
   const handleConfirmAccept = async () => {
     if (!selectedRoom) {
-      toast.error("No room selected!");
+      toast.error('No room selected!');
       return;
     }
 
@@ -74,12 +74,12 @@ const DepositRoom = () => {
     try {
       await acceptDepositRoom(selectedRoom._id); // Assuming you pass room ID to accept
 
-      toast.success("Deposit room accepted successfully.");
+      toast.success('Deposit room accepted successfully.');
       setIsModalVisible(false); // Close modal
       fetchDepositedRooms(); // Refresh the list of rooms after accepting
     } catch (error) {
-      console.error("Error accepting deposit room:", error);
-      toast.error("An error occurred while accepting the deposit room.");
+      console.error('Error accepting deposit room:', error);
+      toast.error('An error occurred while accepting the deposit room.');
     } finally {
       setConfirmLoading(false); // Stop loading after the action is completed
     }
@@ -97,7 +97,7 @@ const DepositRoom = () => {
 
   const handleRejectConfirm = async () => {
     if (!reasonForCancel) {
-      toast.error("Please provide a reason for rejection");
+      toast.error('Please provide a reason for rejection');
       return;
     }
 
@@ -111,11 +111,11 @@ const DepositRoom = () => {
         `Deposit request for room ${selectedRoom.roomNumber} has been rejected.`
       );
       setIsRejectModalOpen(false);
-      setReasonForCancel("");
+      setReasonForCancel('');
       fetchDepositedRooms();
     } catch (error) {
-      console.error("Error rejecting deposit room:", error);
-      toast.error("An error occurred while rejecting the deposit room.");
+      console.error('Error rejecting deposit room:', error);
+      toast.error('An error occurred while rejecting the deposit room.');
     } finally {
       setRejectLoading(false); // Set loading back to false once the rejection is done
     }
@@ -123,40 +123,40 @@ const DepositRoom = () => {
 
   const handleCancelRejectModal = () => {
     setIsRejectModalOpen(false);
-    setReasonForCancel("");
+    setReasonForCancel('');
   };
 
   const columns = [
     {
-      title: "Name",
-      dataIndex: "name",
-      key: "name",
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
     },
     {
-      title: "Room Number",
-      dataIndex: "roomNumber",
-      key: "roomNumber",
+      title: 'Room Number',
+      dataIndex: 'roomNumber',
+      key: 'roomNumber',
     },
     {
-      title: "Amount",
-      dataIndex: "amount",
-      key: "amount",
-      render: (price) => (price ? formatAmount(price) : "N/A"),
+      title: 'Amount',
+      dataIndex: 'amount',
+      key: 'amount',
+      render: (price) => (price ? formatAmount(price) : 'N/A'),
     },
     {
-      title: "Status",
-      dataIndex: "status",
-      key: "status",
+      title: 'Status',
+      dataIndex: 'status',
+      key: 'status',
       render: (status) => (
         <Tag
           color={
-            status === "pending"
-              ? "orange"
-              : status === "accepted"
-              ? "green"
-              : status === "deleted"
-              ? "volcano"
-              : "red"
+            status === 'pending'
+              ? 'orange'
+              : status === 'accepted'
+              ? 'green'
+              : status === 'deleted'
+              ? 'volcano'
+              : 'red'
           }
         >
           {status}
@@ -164,36 +164,36 @@ const DepositRoom = () => {
       ),
     },
     {
-      title: "Rental Time",
-      dataIndex: "rentalTime",
-      key: "rentalTime",
+      title: 'Rental Time',
+      dataIndex: 'rentalTime',
+      key: 'rentalTime',
     },
     {
-      title: "Start Date",
-      dataIndex: "startDate",
-      key: "startDate",
+      title: 'Start Date',
+      dataIndex: 'startDate',
+      key: 'startDate',
     },
     {
-      title: "End Date",
-      dataIndex: "endDate",
-      key: "endDate",
+      title: 'End Date',
+      dataIndex: 'endDate',
+      key: 'endDate',
     },
     {
-      title: "Action",
+      title: 'Action',
       render: (record) =>
-        record.status === "pending" && (
+        record.status === 'pending' && (
           <div className="flex gap-3 items-center">
             <Button
-              title={"Reject"}
+              title={'Reject'}
               iconPosition="left"
               btnReject
               size="large"
-              style={{ backgroundColor: "red", color: "white", border: "none" }}
+              style={{ backgroundColor: 'red', color: 'white', border: 'none' }}
               onClick={() => handleReject(record)} // Open reject modal
             ></Button>
 
             <Button
-              title={"Accept"}
+              title={'Accept'}
               size="large"
               btnAccept
               className="text-white"
@@ -236,7 +236,7 @@ const DepositRoom = () => {
             rules={[
               {
                 required: true,
-                message: "Please enter a reason for rejection",
+                message: 'Please enter a reason for rejection',
               },
             ]}
           >
@@ -244,7 +244,7 @@ const DepositRoom = () => {
               placeholder="Enter reason for rejection"
               value={reasonForCancel}
               onChange={(e) => setReasonForCancel(e.target.value)}
-              style={{ width: "100%", height: "100px" }}
+              style={{ width: '100%', height: '100px' }}
             />
           </Form.Item>
         </Form>
