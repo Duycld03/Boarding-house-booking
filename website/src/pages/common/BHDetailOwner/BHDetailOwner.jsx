4fbf8df0285007bc0330dcaafbe615e6acd05ef7
HongY1402 <todohongy@gmail.com>
2025-06-23 22:31:55 +0700

    feat(blog): fix UI

--- website/src/pages/common/BHDetailOwner/BHDetailOwner.jsx
+++ website/src/pages/common/BHDetailOwner/BHDetailOwner.jsx
@@ -334,6 +334,11 @@ const BHDetailOwner = () => {
         setLoading(false);
         return;
       }
+      if (updatedData.otherImages.length > 15) {
+        toast.error(t('errors.maxOtherImages'));
+        setLoading(false);
+        return;
+      }
 
       const payload = new FormData();
 
